2dac48c241777d330a6b5bb7fa52381074399684
lalkl4 <danila2zhiltsov@gmail.com>
2022-11-26 00:04:27 +0700

    red background body

--- main.go
+++ main.go
@@ -249,7 +249,7 @@ var html = `<!DOCTYPE html>
         margin: 0;
         width: 100%%;
         height: 100%%;
-        background: gray;
+        background: red;
     }
     #log {
         background: white;
